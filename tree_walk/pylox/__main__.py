0ae1446d1d2fd96d73719daa05b9ab9546364e80
John Asper <agentx1994@gmail.com>
2023-11-08 19:02:30 -0500

    Adds Reverse Polish Notation printer

--- tree_walk/pylox/__main__.py
+++ tree_walk/pylox/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 
-from pylox.debug_print import DebugAstPrinter
+from pylox.debug_print import DebugAstPrinter, DebugRpnPrinter
 
 
 from . import PyLoxInterpreter
@@ -12,6 +12,7 @@ def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('script')
     arg_parser.add_argument('--debug-expression', action='store_true')
+    arg_parser.add_argument('--debug-rpn', action='store_true')
     args = arg_parser.parse_args()
     if args.debug_expression:
         expression = BinaryExpression(
@@ -23,6 +24,21 @@ def main():
             GroupingExpression(LiteralExpression(45.67))
         )
         print(DebugAstPrinter().print(expression))
+    elif args.debug_rpn:
+        expression = BinaryExpression(
+            BinaryExpression(
+                LiteralExpression(1),
+                Token(TokenType.PLUS, '+', None, 1),
+                LiteralExpression(2)
+            ),
+            Token(TokenType.STAR, '*', None, 1),
+            BinaryExpression(
+                LiteralExpression(4),
+                Token(TokenType.MINUS, '-', None, 1),
+                LiteralExpression(3)
+            )
+        )
+        print(DebugRpnPrinter().print(expression))
     else:
         interpreter = PyLoxInterpreter()
         interpreter.run_file(args.script)
--- tree_walk/pylox/debug_print.py
+++ tree_walk/pylox/debug_print.py
@@ -23,3 +23,25 @@ class DebugAstPrinter(Visitor[str]):
     def _parenthesize(self, name: str, *items: Expression) -> str:
         items_str = ' '.join(item.accept(self) for item in items)
         return f'({name} {items_str})'
+    
+class DebugRpnPrinter(Visitor[str]):
+    def print(self, expression: Expression) -> str:
+        return expression.accept(self)
+
+    def visit_binary(self, expression: BinaryExpression) -> str:
+        left_str = expression.left.accept(self)
+        right_str = expression.right.accept(self)
+        return f'{left_str} {right_str} {expression.operator.lexeme}'
+    
+    def visit_unary(self, expression: UnaryExpression) -> str:
+        right_str = expression.right.accept(self)
+        return f'{right_str} {expression.operator.lexeme}'
+
+    def visit_literal(self, expression: LiteralExpression) -> str:
+        if expression.value is None:
+            return 'nil'
+        else:
+            return str(expression.value)
+
+    def visit_grouping(self, expression: GroupingExpression) -> str:
+        return expression.expression.accept(self)
